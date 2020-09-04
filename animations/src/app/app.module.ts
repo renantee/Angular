e2fb9bde929df2d13ac4aba61cb2589efbe14600
Renante D. Entera <renantee@codev.com>
2020-09-04 11:44:47 +0800

    Adding offline capabilities with service workers
    Need to execute the following:
    > ng add @angular/pwa
    > ng build --prod
    > npm install -g http-server
    > http-server -p 8080 -c-1 dist/animations

--- animations/src/app/app.module.ts
+++ animations/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, BrowserAnimationsModule],
+  imports: [BrowserModule, FormsModule, BrowserAnimationsModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   providers: [],
   bootstrap: [AppComponent]
 })
